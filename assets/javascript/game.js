99807e0228ca05be3ba71a5aba4343a4da3caeb8
Jchn <jchn.wrk@gmail.com>
2018-10-20 08:21:03 -0400

    v1 commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Character Objects
+var character = [ {
+    name:"magikarp",
+    health:120,
+    attack:8, 
+    counterAttack:15,
+    image: "../images/Magikarp1.png",
+}, {
+    name: "Snorlax",
+    health:100,
+    attack:14, 
+    counterAttack:5,
+    image: "../images/Snorlax1.png",
+}, {
+    name: "psyduck",
+    health:150,
+    attack:8, 
+    counterAttack:20,
+    image: "../images/Psyduck.png",
+},{
+    name:"metapod",
+    health:180,
+    attack:7, 
+    counterAttack:25,
+    image: "../images/Metapod1.png",
+}]
+
+for (i=0; i< character.length; i++); {
+
+     //   $("myCharacter").html(<img src= "character.image[i]"></img>);
+
+    console.log(character.images[i]);
+    
+      };
+
+
+//Window load
+
+
