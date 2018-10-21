5efe54f833f12093e340caa0ebaad77492be2c3d
Jchn <jchn.wrk@gmail.com>
2018-10-20 20:11:42 -0400

    v2 commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,54 @@
 // Character Objects
 var character = [ {
-    name:"magikarp",
+    name:"Magikarp",
     health:120,
     attack:8, 
     counterAttack:15,
-    image: "../images/Magikarp1.png",
 }, {
     name: "Snorlax",
     health:100,
     attack:14, 
     counterAttack:5,
-    image: "../images/Snorlax1.png",
 }, {
-    name: "psyduck",
+    name: "Psyduck",
     health:150,
     attack:8, 
     counterAttack:20,
-    image: "../images/Psyduck.png",
 },{
-    name:"metapod",
+    name:"Metapod",
     health:180,
     attack:7, 
     counterAttack:25,
-    image: "../images/Metapod1.png",
 }]
 
-for (i=0; i< character.length; i++); {
+for (var i = 0; i < character; i++); {
 
-     //   $("myCharacter").html(<img src= "character.image[i]"></img>);
+};
 
-    console.log(character.images[i]);
-    
-      };
+function addproperties() {
+    $("#info").append("Name: " + character[i].name + "<br>");
+    $("#info").append("Health: " + character[i].health + "<br>");
+    $("#info").append("Attack: " + character[i].attack + "<br>");
+    $("#info").append("Counter: " + character[i].counterAttack + "<br>"); 
+}
+
+ $("#magiKarp").click(function() {
+     i=0; addproperties();
+      });
+
+ $("#snorLax").click(function() {
+     i=1; addproperties();
+      });
+
+ $("#psyDuck").click(function() {
+     i=2; addproperties();
+      });
+
+ $("#metaPod").click(function() {
+     i=3; addproperties();; 
+      });
+
+
 
 
 //Window load
